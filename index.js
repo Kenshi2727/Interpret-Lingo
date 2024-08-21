20b482eece43924a3493331796ee731b1c43be1b
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-08-22 00:54:29 +0530

    updated changes interpret lingo

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ import bodyParser from 'body-parser';
 import axios from 'axios';
 const app=express();
 const port=3000;
-
+let lang='en';
 app.use(express.static(('public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/",(req,res)=>{res.render('index.ejs')});
+app.post('/lang',(req,res)=>{
+    lang=req.body.lang;
+    console.log(req.body.lang);
+    
+    res.render('index.ejs');
+})
 app.post('/translate',async (req,res)=>{
      try{
-       const response = await axios.get("");
-       const result = response.data;
+      const text=req.body.from;
+      const response = await axios.post("https://rapid-translate-multi-traduction.p.rapidapi.com/t");
+      const result = response.data;
      }
     catch(error){
         console.error(`Error: ${error}`);
@@ -20,3 +27,5 @@ app.post('/translate',async (req,res)=>{
     }
 });
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
+
+
