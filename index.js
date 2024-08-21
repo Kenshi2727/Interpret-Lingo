7eaf932a29c88e49b9d1c57deeb96d6750d98277
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-08-21 18:49:16 +0530

    edited textarea and tested api fetch request

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import path from 'path';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 const app=express();
 const port=3000;
 
 app.use(express.static(('public')));
-//default use
-app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/",(req,res)=>{res.render('index.ejs')});
+app.post('/translate',async (req,res)=>{
+     try{
+       const response = await axios.get("");
+       const result = response.data;
+     }
+    catch(error){
+        console.error(`Error: ${error}`);
+        res.send("Some Error Occured!😭");
+    }
+});
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
